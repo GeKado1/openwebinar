5a3ef6995e579b690a06c8f66c20af6ec6a9c494
GeKado1 <98519650+GeKado1@users.noreply.github.com>
2022-06-14 09:50:50 +0200

    Add files via upload

--- objetos.js
+++ objetos.js
@@ -13,3 +13,9 @@ jugador.incrementarFuerza();
 jugador.consultarFuerza();*/
 
 var date = new Date();
+
+var anyo = date.getFullYear();
+var mes = date.getMonth() + 1;
+var dia = date.getDay();
+
+console.log("Hola, hoy es " + dia + " del mes " + mes + " del año " + anyo);
