a98f70e0f7ad9f2d474f2c4fad8f2843c0cf8df8
GeKado1 <98519650+GeKado1@users.noreply.github.com>
2022-06-14 10:11:46 +0200

    math

--- objetos.js
+++ objetos.js
@@ -18,4 +18,22 @@ var anyo = date.getFullYear();
 var mes = date.getMonth() + 1;
 var dia = date.getDay();
 
-console.log("Hola, hoy es " + dia + " del mes " + mes + " del año " + anyo);
+//console.log("Hola, hoy es " + dia + " del mes " + mes + " del año " + anyo);
+
+var pi = Math.PI;
+
+/*console.log(pi);
+console.log(Math.min(1, 2, 3, 4345, 0, -10));
+console.log(Math.max(1, 2, 3, 4345, 0, -10));
+console.log(Math.round(4.4));
+console.log(Math.floor(4.8));
+console.log(Math.ceil(4.2));*/
+
+var aleatorio = Math.random();
+//console.log(aleatorio);
+
+function random(numMax) {
+    return Math.round(Math.random() * numMax);
+}
+
+console.log(random(10));
