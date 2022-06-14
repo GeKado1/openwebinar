f28431f6bf8874b02e3a3994b450ea2bf97de407
GeKado1 <98519650+GeKado1@users.noreply.github.com>
2022-06-14 10:32:08 +0200

    arrays

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # openwebinar
 
+(Los ejemplos están comentados, si quieres ver como funciona puedes descomentar)
  Objetos
 1) Jugador y sus funciones
 2) Fecha entera y fecha por partes
 3) Pi, math.min/max, math.round/floor/ceil, math.random y función random con núm max
+4) Arrays instanciados de diversos modos y como llamarlo
--- objetos.js
+++ objetos.js
@@ -1,3 +1,4 @@
+/* 1 */
 var jugador = {
     fuerza: 1,
     incrementarFuerza: function() {
@@ -7,19 +8,19 @@ var jugador = {
         console.log("Tu fuerza es de: " + this.fuerza);
     }
 };
-
 /*jugador.consultarFuerza();
 jugador.incrementarFuerza();
 jugador.consultarFuerza();*/
 
+/* 2 */
 var date = new Date();
-
 var anyo = date.getFullYear();
 var mes = date.getMonth() + 1;
 var dia = date.getDay();
-
+//console.log(date);
 //console.log("Hola, hoy es " + dia + " del mes " + mes + " del año " + anyo);
 
+/* 3 */
 var pi = Math.PI;
 
 /*console.log(pi);
@@ -35,5 +36,28 @@ var aleatorio = Math.random();
 function random(numMax) {
     return Math.round(Math.random() * numMax);
 }
+//console.log(random(10));
 
-console.log(random(10));
+/* 4 */
+var array1 = [
+    'Primer elemento',
+    'Segundo elemento',
+    11,
+    true,
+    {key: "Soy el quinto elemento"},
+    null
+];
+
+var array2 = new Array(3);
+var array3 = new Array("rojo", "azul", "verde", "amarillo");
+
+console.log(array3);
+console.log(array3[0]);
+console.log(array3[1]);
+console.log(array3[2]);
+console.log(array3[3]);
+console.log(array3[4]); //posició sin valor definido
+
+array3.push("negro");
+console.log(array3);
+console.log(array3[4]) //ahora tiene valor
