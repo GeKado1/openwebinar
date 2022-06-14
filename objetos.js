d27d88cef9659d45b8626344db13424e6b33553a
GeKado1 <98519650+GeKado1@users.noreply.github.com>
2022-06-14 09:41:12 +0200

    initial commit

---
+++ objetos.js
@@ -0,0 +1,15 @@
+var jugador = {
+    fuerza: 1,
+    incrementarFuerza: function() {
+        this.fuerza++;
+    },
+    consultarFuerza: function() {
+        console.log("Tu fuerza es de: " + this.fuerza);
+    }
+};
+
+/*jugador.consultarFuerza();
+jugador.incrementarFuerza();
+jugador.consultarFuerza();*/
+
+var date = new Date();
