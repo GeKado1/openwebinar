ae38456f79659a35ba72fb005e30e84ac5141d14
GeKado1 <98519650+GeKado1@users.noreply.github.com>
2022-06-14 11:25:06 +0200

    ejercicios arrays

---
+++ ejercicios/ejArrays.js
@@ -0,0 +1,18 @@
+/* 1 */
+var arrayEj = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+console.log("Los números pares son:")
+for (var i = 0; i < arrayEj.length; i++) {
+    if (arrayEj[i]%2 == 0) {
+        console.log(arrayEj[i]);
+    }
+}
+
+/* 2 */
+var resultadoSuma = 0;
+
+console.log("La suma de todos los números es:")
+for (var i = 0; i < arrayEj.length; i++) {
+    resultadoSuma = resultadoSuma + arrayEj[i];
+}
+console.log(resultadoSuma);
